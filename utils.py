100cc316699cd06f039011140c079b5b1c9828f7
aastopher <aaron.stopher@gmail.com>
2022-10-27 16:44:18 -0600

    map location to current device on load

--- utils.py
+++ utils.py
@@ -57,7 +57,8 @@ def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
 
 def load_checkpoint(checkpoint_file, model, optimizer, lr):
     print("=> Loading checkpoint")
-    checkpoint = torch.load(checkpoint_file, map_location="cuda")
+    # checkpoint = torch.load(checkpoint_file, map_location="cuda")
+    checkpoint = torch.load(checkpoint_file, map_location=config.DEVICE)
     model.load_state_dict(checkpoint["state_dict"])
     optimizer.load_state_dict(checkpoint["optimizer"])
 
